4eb779be9d4c82406aece1054cd7ffbd2466690a
Letícia Vitória dos Santos <36903895+LeticiaVSantos@users.noreply.github.com>
2018-04-04 20:24:51 -0300

    Teste

---
+++ src/java/br/com/fatecpg/ads/poo/project/HomeServlet.java
@@ -0,0 +1,215 @@
+package br.com.fatecpg.ads.poo.project;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Leticia
+ */
+@WebServlet(urlPatterns = {"/home.php"})
+public class HomeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+             /*Tag meta Responsiva*/
+            out.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+             /*Google fonts - footer*/
+            out.println("<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>");
+             /*Favicon*/
+            out.println("<link rel=\"shortcut icon\" href=\"Imagens/favicon (1)_1.ico\" />");
+            
+            out.println("<title> JurosWeb </title>");    
+             /*css - Bootstrap*/
+            out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">");
+            out.println("</head>");
+            out.println("<body style=\"background-color: black;\">");  
+            
+             /*inicio NavBar*/
+            out.println("<nav class=\"navbar sticky-top navbar-dark bg-dark\">\n" +
+            "<a class=\"navbar-brand\" href=\"home.php\">\n" +         
+            "<img src=\"Imagens/cifrao.png\" width=\"30\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">\n" +
+            "JurosWeb\n" +       
+            "</a>\n" +  
+            "<span class='navbar-text'>"+
+            "JurosWeb, a solução."+
+            "</span>"+
+            "</nav>");
+            
+            out.println("<br>");
+            
+             /*Inicio Jumbotron*/
+            out.println("<div class=\"jumbotron jumbotron-fluid\" style=\"background-color: lightseagreen;\">\n" +
+            "<div class=\"container\">\n" +
+            "<h1 class=\"display-4\">JurosWeb</h1>\n" +
+            "<p class=\"lead\">Uma maneira simples e eficiente para o cálculo de juros simples e composto.</p>\n" +
+            "</div>\n" +
+            "</div>");
+            
+             /*Inicio Cards de calculo de juros*/
+            out.println("<div class='row' style=\"width: 80%;position: relative;left: 7%;\">" + 
+            "<div class='col-sm-6'>" +
+            "<div class=\"card border-primary mb-3\" style=\"max-width: 30rem;\"\n >" +
+            "<div class='card-body' style=\"background-color: lightsteelblue;border-color: black;\">" +
+            "<center><h5 class='card-title'>Juros Simples</h5></center>" +
+            "<hr>"+            
+            "<center><p class='card-text'>É muito utilizado do dia a dia quando emprestamos dinheiro a outra pessoa, por exemplo." +
+            " Uma espécie de \"te empresto, mas quero que me pague a mais por isso\".</p></center>" + 
+            "<br>" +       
+            "<center><a href='juros-simples.php' class='btn btn-primary' style=\"background-color: lightseagreen;border-color: lightseagreen;color: black;\">Calcular</a></center>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+                    
+            "<div class='col-sm-6' style=\"position: relative;left: 13%;\">" +
+            "<div class=\"card border-secondary mb-3\" style=\"max-width: 30rem;\" \n >" +
+            "<div class='card-body' style=\"background-color: lightsteelblue;border-color: black;\">" +
+            "<center><h5 class='card-title'>Juros Composto</h5></center>" +
+            "<hr>"+            
+            "<center><p class='card-text'>São utilizados pelo sistema financeiro, pois oferece uma rentabilidade melhor." +
+            " A taxa de juros é sempre aplicada ao somatório do capital no final do mês. </p></center>"+ 
+            "<br>" +        
+            " <center><a href='juros-composto.php' class='btn btn-primary' style=\"background-color: lightseagreen;border-color: lightseagreen;color: black;\">Calcular</a></center>" +
+            " </div>" +
+            "</div>" +
+            "</div>" +
+            "</div>");
+            
+            
+            out.println("<br>");
+            
+            /*Alert - Usado para divulgar os desenvolvedores*/
+            out.println("<div class=\"alert alert-success\" role=\"alert\" style=\"background-color: lightblue;border-color: black;\">\n" +
+            "  <center> Conheça nossos desenvolvedores!</center>\n" +
+            "</div>");
+            out.println("<br>");  
+                 
+            /*Inicio Cards de desenvolvedores*/
+            
+            out.println("<div class=\"card-deck\" style=\"width: 100%;position: relative;left: 1%;\">\n" +
+            "  <div class=\"card border-primary mb-3\" style=\"background-color: #ced5e5;\">\n" +
+            "<br>" + 
+            "    <center><img class=\"rounded-circle\" class=\"card-img-top\" src=\"Imagens/Allan.jpg\" alt=\"Allan Patrick\" style=\"width:50%;position: relative;bottom: -5%;\"></center>\n" +
+            "    <div class=\"card-body\" style=\"background-color: #ced5e5;\">\n" +
+            "      <center><h5 class=\"card-title\">Allan Patrick</h5></center>\n" +
+            "      <center><p class=\"card-text\"> Estutande de Análise e Desenvolvimento de Sistemas Fatec - Praia Grande.</p></center>\n" +
+            "      <center><p class=\"card-text\"> Conheça também seus projetos no GitHub.</p></center><br>\n" +        
+            "      <center><a href=\"https://github.com/allanpatrick1\" <button class=\"btn btn-primary\" style=\"background-color: lightseagreen;border-color: lightseagreen;color: black;\">GitHub</button></a></center>"+       
+            "    </div>\n" +
+            "  </div>\n" +
+                    
+            "  <div class=\"card border-primary mb-3\" style=\"background-color: #ced5e5;\">\n" +
+            "<br>" +         
+            "    <center><img class=\"rounded-circle\" class=\"card-img-top\" src=\"Imagens/Andrey.jpg\" alt=\"Andrey Antunes\"  style=\"width:50%;position: relative;bottom: -5%;\"></center>\n" +
+            "    <div class=\"card-body\" style=\"background-color: #ced5e5;\">\n" +
+            "      <center><h5 class=\"card-title\">Andrey Antunes</h5></center>\n" +
+            "      <center><p class=\"card-text\">Estutande de Análise e Desenvolvimento de Sistemas Fatec - Praia Grande.</p></center>\n" +
+            "      <center><p class=\"card-text\"> Conheça também seus projetos no GitHub.</p></center><br>\n" +        
+            "       <center><a href=\"https://github.com/AndreyAntunes\" <button class='btn btn-primary' style=\"background-color: lightseagreen;border-color: lightseagreen;color:black;\">GitHub</button></a></center>"+
+            "    </div>\n" +
+            "  </div>\n" +
+             
+                  
+            "  <div class=\"card border-primary mb-3\" style=\"background-color: #ced5e5;\">\n" +
+            "<br>" +                    
+            "    <center><img  class=\"rounded-circle\" class=\"card-img-top\" src=\"Imagens/Leticia (2).jpg\" alt=\"Leticia Santos\" style=\"width:50%;position: relative;bottom: -5%;\"></center>\n" +
+            "    <div class=\"card-body\" style=\"background-color: #ced5e5;\">\n" +
+            "      <center><h5 class=\"card-title\">Leticia Santos</h5></center>\n" +
+            "      <center><p class=\"card-text\">Estutande de Análise e Desenvolvimento de Sistemas Fatec - Praia Grande.</p></center>\n" +
+            "      <center><p class=\"card-text\"> Conheça também seus projetos no GitHub.</p></center><br>\n" +
+            "       <center><a href=\"https://github.com/LeticiaVSantos\" <button class=\"btn btn-primary\" style=\"background-color: lightseagreen;border-color: lightseagreen;color: black;\" >GitHub</button></a></center>"+        
+            "    </div>\n" +
+            "  </div>\n" +
+            "</div>");
+            
+             out.println("<hr>");
+            
+         
+                    
+            out.println("<hr><center><footer class=\"footer\">" +
+            "<br>" +
+                    
+            "<h5>JurosWeb - A Solução.</h5>" +
+            "<p>Possui alguma sugestão? Entre em contato pelo nosso email: <a href=\"mailto:example@example.com\">suporte@jurosweb.com.br</a></p>" +
+            "<p>© 2018 Copyright:</p>\n" +
+            "\"<a href='https://github.com/LeticiaVSantos/Projeto-02---POO'</a>Projeto Servlets"+  
+                    
+            "</footer></center>");
+            
+            
+            
+            out.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n");
+            out.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n");
+            out.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/br/com/fatecpg/ads/poo/project/jurosComposto.java
@@ -0,0 +1,221 @@
+package br.com.fatecpg.ads.poo.project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat; 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Allan P. Cabral
+ */
+
+@WebServlet(name = "juros-composto.php", urlPatterns = {"/juros-composto.php"})
+public class jurosComposto extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+            
+            try (PrintWriter out = response.getWriter()) {
+                // Inicialização do HTML
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                
+                // Tag Responsiva
+                out.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+                
+                // Carregando Google Fonts*/
+               out.println("<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>");
+                
+                // Carregando FavIcon
+                out.println("<link rel=\'shortcut icon\' href=\'Imagens/favicon (1).ico\' />");
+                out.println("<title> JurosWeb </title>");
+                
+                // Carregando o Css/Bootstrap
+                out.println("<link rel=\'stylesheet\' "
+                            + "href=\'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\'\n "
+                            + "integrity=\'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\'\n "
+                            + "crossorigin=\'anonymous\'>");      
+                out.println("</head>");
+                
+                // Inicio do Body / Navbar
+                out.println("<body style=\'background-color: black;\'>");
+                out.println("<nav class=\'navbar sticky-top navbar-dark bg-dark\'>\n " 
+                            + "<a class=\'navbar-brand\' href=\'home.php\'>\n "
+                            + "<img src=\'Imagens/cifrao.png\' width=\'30\' height=\'30\' "
+                            + "class=\'d-inline-block align-top\' alt=\'\'> JurosWeb </a>\n " 
+                            + "<span class='navbar-text'>JurosWeb, a solução.</span> "
+                            + "</nav>");
+                
+                // Descrição da pagina
+                out.println("<center>\n"
+                            + "<h1 style=\'text-shadow: 1px 0px 0px #122b68, -1px 0px 0px #122b68, "
+                            + "0px 1px 0px #122b68, 0px -1px 0px #122b68;font-family: tahoma;color: white; "
+                            + "position: relative;width: 35%;top: 10% !important;\'> "
+                            + "Cálculo de Juros Composto</h1>\n "
+                            + "<hr/ style=\'background-color: #b8c1c1;\'>\n "
+                            + "</center>");
+                
+                // Formulario
+                out.println("<center>\n"
+                            + "<form style=\'text-shadow: 1px 0px 0px #122b68, -1px 0px 0px #122b68, "
+                            + "0px 1px 0px #122b68, 0px -1px 0px #122b68;font-family: tahoma;color: white; "
+                            + "position: relative; width: 20%;\'>\n " 
+                            + "<div class='row'>\n "
+                            
+                            // Entrada de aplicação
+                            + "<div class='col'> " 
+                            + "Aplicação (R$):\n "
+                            + "<input type='text' style=\'width: 200px;\' class='form-control' "
+                            + "placeholder='Ex: 100' name='valor'/>\n<br></div>\n "
+                            
+                            // Entrada de periodo
+                            + "<div class='col'> "
+                            + "Período (Em Meses):\n "
+                            + "<input type='text' style=\'width: 200px;\' class='form-control' "
+                            + "placeholder='Ex: 6' name='meses'/>\n<br></div>\n "
+                            
+                            // Entrada de Taxa
+                            + "<div class='col'> "
+                            + "Taxa de Juros (%):\n "
+                            + "<input type='text' style=\"width: 200px;\" class='form-control' "
+                            + "placeholder='Ex: 10' name='juros'/>\n<br></div>\n " 
+                            
+                            // Botão Submit 
+                            + "<input class=\'btn btn-primary\' type=\'submit\' "
+                            + "style=\'width: 200px;position:relative;left:16%;background-color: lightseagreen; "
+                            + "border-color: lightseagreen;color:black;\' "
+                            + "value='Calcular'/></div>\n "
+                            
+                            + "</div>\n</form>\n</center><br>\n");
+            
+            // Configuração do Botão Submit
+            try{
+                // Formato das casas decimais
+                DecimalFormat df = new DecimalFormat("0.##");
+                int meses = Integer.parseInt(request.getParameter("meses"));
+                double valor = Double.parseDouble(request.getParameter("valor")); 
+                double juros = Double.parseDouble(request.getParameter("juros")); 
+                double taxa;
+                                               
+                  
+                // Regra para impedir campo vazio / TRUE
+                if(valor>0 && juros>0 && meses>0){
+                    out.println("<div class=\"container\"><center><table class=\"table\">");
+                    out.println("<thead class=\"thead-dark\"><tr class=\"table-active\">");
+                    out.println("<th scope=\"row\"> <center>Mês</center </th><th scope=\"row\"> <center>Valor da Parcela</center> </th>");
+                    out.println("</tr></thead>");
+                    for (int i=0; i<meses; i++) {
+                        taxa = valor * (juros/100);
+                        valor = valor + taxa;
+                        
+                        String total = df.format(valor);
+                        
+                        /*tabela - resultado do montante */
+                        out.println("<tbody><tr class=\"container table table-hover\"><td><center>"+(i+1)+"</center></td><td><center>R$ "+total+"</center></td></tr>");
+                        
+                    }
+                    
+                    out.println("</table></center><div class=\"container\">");
+                    // Limitando as casas decimais da variavel Valor
+                    String total = df.format(valor);
+                    
+                }
+                
+                // Regra / FALSE
+                else{
+                    out.println("<br>");
+                    out.println("<center style=\"text-shadow: 1px 0px 0px #122b68, -1px 0px 0px #122b68, "
+                                + "0px 1px 0px #122b68, 0px -1px 0px #122b68;font-family: tahoma; "
+                                + "color: white;position: relative;left: 2%;width: 96%;\"> "
+                                + "Envio não concluído. Preencha os campos com valores positivos.</center>");
+                    out.println("<hr/ style=\"background-color: #b8c1c1;\">");
+                }
+            }
+            
+                // Regra de exceção
+                catch(NumberFormatException nfe){
+                    out.println("<br>");
+                    out.println("<center style=\"text-shadow: 1px 0px 0px #122b68, -1px 0px 0px #122b68, "
+                                + "0px 1px 0px #122b68, 0px -1px 0px #122b68;font-family: tahoma;"
+                                + "color: white;position: relative;left: 2%;width: 96%;\">"
+                                + "Lembre-se: Preencha os campos com numeros válidos.</center>");
+                    out.println("<hr/ style=\"background-color: #b8c1c1;\">");
+                }
+                
+                // Botão de Home/JSimples
+                out.println("<a href='home.php'class=\"btn btn-primary btn-lg active\" "
+                            + "role=\"button\" style=\"position: relative;top: 0%;left: 2%;background-color: lightseagreen;"
+                            + "border-color: lightseagreen;color:black;\" aria-pressed=\"true\">Voltar</a>");
+                out.println("<a href='juros-simples.php' class=\"btn btn-secondary btn-lg active\" "
+                            + "role=\"button\" aria-pressed=\"true\"style=\"left: 3%;position: relative;background-color: lightseagreen;"
+                            + "border-color: lightseagreen;color:black;\">Juros Simples</a>");
+                out.println("<br><br>");
+                
+                            /* Footer */
+                
+                             out.println("<hr><center><footer class=\"footer\">" +
+                             "<br>" +
+                    
+                             "<h5>JurosWeb - A Solução.</h5>" +
+                             "<p>Possui alguma sugestão? Entre em contato pelo nosso email: <a href=\"mailto:example@example.com\">suporte@jurosweb.com.br</a></p>" +
+                             "<p>© 2018 Copyright:</p>\n" +
+                             "<a href='https://github.com/LeticiaVSantos/Projeto-02---POO'</a> Projeto Servlets"+  
+                    
+                             "</footer></center>");
+                                    
+                /* Java Script - Bootstrap */
+                out.println("<script src=\'https://code.jquery.com/jquery-3.2.1.slim.min.js\' integrity=\'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\' crossorigin=\'anonymous\'></script>");
+                out.println("<script src=\'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\' integrity=\'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\' crossorigin=\'anonymous\'></script>");
+                out.println("<script src=\'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\' integrity=\'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\' crossorigin=\'anonymous\'></script>");
+            
+                out.println("</body>");
+                out.println("</html>");
+            }
+        }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
